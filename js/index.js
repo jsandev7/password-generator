59b6f74ea37300e62da9c24eedc66121eec2ada4
jsandev7 <jsandev@outlook.com>
2025-10-27 11:49:29 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+const MAX_LETTER_INDEX = 51
+const LENGTH_OF_ARRAY_OF_CHARACTERS = characters.length
+
+// Generar numeros aleatorios
+function generateRandomIntNumber(minIntNumber, maxIntNumber) {
+    let randomIntNumber = null
+
+    randomIntNumber = Math.floor(Math.random() * (maxIntNumber - minIntNumber + 1) + minIntNumber);
+
+    return randomIntNumber
+}
+
+// Funcion generar password
+function generatePassword(arrayOfElements, sizeOfPassword = 15, includeNumbersAndSymbols = true) {
+    let generatedPassword = ""
+    
+    if (includeNumbersAndSymbols) {
+        for (let i = 0; i < sizeOfPassword; i++) {
+            generatedPassword += arrayOfElements[generateRandomIntNumber(0, LENGTH_OF_ARRAY_OF_CHARACTERS - 1)]
+        }
+    } else {
+        for (let i = 0; i < sizeOfPassword; i++) {
+            generatedPassword += arrayOfElements[generateRandomIntNumber(0, MAX_LETTER_INDEX)]
+        }
+    }
+
+    return generatedPassword
+}
+
+document.querySelectorAll(".password-result")[0].textContent = generatePassword(characters, 15)
+document.querySelectorAll(".password-result")[1].textContent = generatePassword(characters, 15)
+
+
+
+
+
+
+
+
+
